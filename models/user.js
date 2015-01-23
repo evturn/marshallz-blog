36aa689a7537b798b025d45d46a271db49f58610
Evan Turner <evturn@gmail.com>
2015-01-23 14:21:39 -0500

    building User Schema

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
