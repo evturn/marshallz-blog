192e38a55ba81c63956e214e06825d61646ffeb9
Evan Turner <evturn@gmail.com>
2015-01-22 23:11:57 -0500

    backbone collection fetching entries from api

--- public/javascripts/collections/entries.js
+++ public/javascripts/collections/entries.js
@@ -1,7 +1,9 @@
 var Entries = Backbone.Collection.extend({
 	model: Entry,
-	url: '/api/entries'
+	url: '/api/entries',
 	initialize: function() {
 		this.fetch({reset: true});
 	}
 });
+
+entries = new Entries({})
