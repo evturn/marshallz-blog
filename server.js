3310476abe1399e2ace395573fc550d87ecc8bff
Evan Turner <evturn@gmail.com>
2015-01-23 15:30:00 -0500

    authorization with passport required

--- server.js
+++ server.js
@@ -3,29 +3,25 @@ var mongoose   = require('mongoose');
 var path 			 = require('path');
 var logger 		 = require('morgan');
 var bodyParser = require('body-parser');
-var Entry 		 = require('./models/entry');
-var User 		 	 = require('./models/user');
 var entryController = require('./controllers/entry');
 var userController = require('./controllers/user');
+var passport = require('passport');
+var authController = require('./controllers/auth');
 
 mongoose.connect('mongodb://localhost:27017/marshallz-blog');
-
 var app = express();
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+app.use(passport.initialize());
 
-var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get('/', function(require, response) {
 	response.render('index.html');
 });
 
-
-var entriesRoute = router.route('/entries');
 
 router.route('/entries')
 	.post(entryController.postEntries)
@@ -43,6 +39,7 @@ router.route('/users')
 
 app.use('/api', router);
 
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
 	console.log('Listening on localhost:3000');
 });
