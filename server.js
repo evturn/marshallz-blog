4ecf5275b0127f770a73b84e98394b343fd17df8
Evan Turner <evturn@gmail.com>
2015-01-22 21:07:19 -0500

    mongoose installed

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-var express 	 = require('express'),
-var path 			 = require('path'),
-var logger 		 = require('morgan'),
+var express 	 = require('express');
+var mongoose   = require('mongoose');
+var path 			 = require('path');
+var logger 		 = require('morgan');
 var bodyParser = require('body-parser');
+
+
+
 
 var app = express();
 app.use(express.static('public'));
+
+var port = process.env.PORT || 3000;
+var router = express.Router();
 
 app.get('/', function(require, response) {
 	response.render('index.html');
 });
 
 
-app.listen(5000, function() {
+app.listen(port, function() {
 	console.log('Listening on localhost:5000');
 });
