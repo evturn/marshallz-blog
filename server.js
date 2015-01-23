460f7de7c863eb8e56e3b0d07999dd8806af5e2f
Evan Turner <evturn@gmail.com>
2015-01-22 21:12:11 -0500

    express router loading index

--- server.js
+++ server.js
@@ -4,20 +4,23 @@ var path 			 = require('path');
 var logger 		 = require('morgan');
 var bodyParser = require('body-parser');
 
-
-
+mongoose.connect('mongodb://localhost:27017/marshallz-blog');
 
 var app = express();
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 var port = process.env.PORT || 3000;
 var router = express.Router();
 
-app.get('/', function(require, response) {
+router.get('/', function(require, response) {
 	response.render('index.html');
 });
 
 
 app.listen(port, function() {
-	console.log('Listening on localhost:5000');
+	console.log('Listening on localhost:3000');
 });
