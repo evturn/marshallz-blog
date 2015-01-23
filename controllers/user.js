b7ee6de8b74cf9481f285c72d160901f5b33ca72
Evan Turner <evturn@gmail.com>
2015-01-23 15:06:46 -0500

    user controller created

---
+++ controllers/user.js
@@ -0,0 +1,22 @@
+var User = require('../models/user');
+
+exports.postUsers = function(req, res) {
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  user.save(function(err) {
+    if (err)
+      res.send(err);
+    res.json({message: 'Welcome! And more importantly, welcome to Marshallz Blog.'});
+  });
+};
+
+exports.getUsers = function(req, res) {
+  User.find(function(err, users) {
+    if (err)
+      res.send(err);
+    res.json(users);
+  });
+};
