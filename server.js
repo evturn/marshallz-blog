1a719adfc15aaa788df0668e8794b884cee3baed
Evan Turner <evturn@gmail.com>
2015-01-29 01:21:39 -0500

    weather widget added to right column

--- server.js
+++ server.js
@@ -13,16 +13,65 @@ var newEntry				= require('./lib/newEntry.js');
 var app = express();
 mongoose.connect('mongodb://localhost:27017/marshallz-blog');
 
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
+var handlebars = require('express3-handlebars').create({
+	  defaultLayout:'main',
+    helpers: {
+      section: function(name, options){
+        if(!this._sections) this._sections = {};
+        this._sections[name] = options.fn(this);
+        return null;
+       	}
+    }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'))
 
+app.use(function(req, res, next) {
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+	next();
+});
+
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
 app.use(passport.initialize());
+
+
+function getWeatherData(){ 
+	return {
+  	locations: [
+			{
+				name: 'New York City',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '25.0 F (-4.8 C)',
+			},
+			{
+				name: 'Los Angeles',
+				forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '75.0 F (40.8 C)',
+			},
+      {
+      	name: 'Marshallz Blog',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/sunny.gif',
+        weather: 'Overcast',
+        temp: '137.1 F (80.3 C)',
+			},
+		],
+	};
+}
+
+app.use(function(req, res, next) {
+	if(!res.locals.partials) res.locals.partials = {}; 
+	res.locals.partials.weather = getWeatherData(); 
+	next();
+});
 
 app.get('/', function(require, response) {
 	response.render('index');
@@ -33,7 +82,6 @@ app.get('/latest', function(req, res) {
 		entry: newEntry.createContent() 
 	});
 });
-
 
 var router = express.Router();
 
