cc4684ba2651c14d2b8ff72592229caae624150f
Evan Turner <evturn@gmail.com>
2015-01-29 15:33:53 -0500

    views almost completely revised

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,6 +12,24 @@ console.log(entries);
 
         //$("#blog-logo").prependTo("#site-head-content");
 
+
+          // Target your .container, .wrapper, .post, etc.
+            $(".content").fitVids();
+
+          // Target your .container, .wrapper, .post, etc.
+            $(".content").fitVids();
+
     });
 
 }(jQuery));
+
+
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+        var disqus_shortname = 'marshallzblog'; // required: replace wakeghost with your forum shortname
+
+        /* * * DON'T EDIT BELOW THIS LINE * * */
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
