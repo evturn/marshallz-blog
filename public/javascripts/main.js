cf997c3ae4873b761d6749b6b52ab6e391a13562
Evan Turner <evturn@gmail.com>
2015-01-29 14:19:25 -0500

    added scripts to layout

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,3 +2,16 @@ entries = new Entries();
 entriesView = new EntriesView({collection: entries});
 console.log(entries);
 
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+
+    });
+
+}(jQuery));
